f8374c25fd33a74319fc265831dcf1aeafea07d0
nando <muhammad.rezki.ananda@gmail.com>
2021-08-09 20:19:42 +0700

    Create bottom nav

--- app/src/main/java/com/mrrezki/aplikasisederhana/data/view/activity/MainActivity.kt
+++ app/src/main/java/com/mrrezki/aplikasisederhana/data/view/activity/MainActivity.kt
@@ -2,11 +2,19 @@ package com.mrrezki.aplikasisederhana.data.view.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.mrrezki.aplikasisederhana.R
+import com.mrrezki.aplikasisederhana.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        val navController = findNavController(R.id.hostFragment)
+        binding.navigationView.setupWithNavController(navController)
     }
 }
