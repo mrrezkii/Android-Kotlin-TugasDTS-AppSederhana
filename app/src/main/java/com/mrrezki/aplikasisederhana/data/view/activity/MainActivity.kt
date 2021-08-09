8fbc8e65d4aa1ea15c9621acaff2d3034514d892
nando <muhammad.rezki.ananda@gmail.com>
2021-08-09 19:56:50 +0700

    Layouting fragment beranda

--- app/src/main/java/com/mrrezki/aplikasisederhana/data/view/activity/MainActivity.kt
+++ app/src/main/java/com/mrrezki/aplikasisederhana/data/view/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.mrrezki.aplikasisederhana
+package com.mrrezki.aplikasisederhana.data.view.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.mrrezki.aplikasisederhana.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
